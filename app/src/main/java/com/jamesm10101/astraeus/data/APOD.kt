b05ad0fe0745830a520b6af7cd4660d3faba2288
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-07-11 14:37:58 -0400

    APOD data class created

---
+++ app/src/main/java/com/jamesm10101/astraeus/data/APOD.kt
@@ -0,0 +1,14 @@
+package com.jamesm10101.astraeus.data
+
+import com.squareup.moshi.Json
+
+data class APOD(
+    @Json(name = "url") val imgSrcUrl: String,
+    @Json(name = "hdurl") val imgSrcHDUrl: String,
+    @Json(name = "date") val date: String,
+    @Json(name = "title") val title: String,
+    @Json(name = "copyright") val copyright: String?,
+    @Json(name = "explanation") val explanation: String,
+    @Json(name = "media_type") val mediaType: String,
+    @Json(name = "service_version") val serviceVersion: String,
+)
