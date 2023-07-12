55cacfcc716cc1fb70a66ee21e968f503d33e3f6
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-07-11 23:30:43 -0400

    MarsRover data class created

---
+++ app/src/main/java/com/jamesm10101/astraeus/data/MarsRover.kt
@@ -0,0 +1,16 @@
+package com.jamesm10101.astraeus.data
+
+import com.squareup.moshi.Json
+import java.util.*
+
+data class MarsRover(
+    @Json(name = "id") val id: Int,
+    @Json(name = "name") val name: String,
+    @Json(name = "landing_date") val landingDate: String,
+    @Json(name = "launch_date") val launchDate: String,
+    @Json(name = "status") val status: String,
+    @Json(name = "max_sol") val maxSol: Int,
+    @Json(name = "max_date") val maxDate: String,
+    @Json(name = "total_photos") val totalPhotos: Int,
+    @Json(name = "cameras") val cameras: List<MarsRoverCam>
+)
