eea82d73ce6b485c741724ff983e5a0940ba5d36
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-07-16 14:41:31 -0400

    MarsRoverEnums Created

---
+++ app/src/main/java/com/jamesm10101/astraeus/data/MarsRoverEnums.kt
@@ -0,0 +1,21 @@
+package com.jamesm10101.astraeus.data
+
+enum class MarsRoverName {
+    PERSEVERANCE, CURIOSITY, SPIRIT, OPPORTUNITY
+}
+
+enum class PerseveranceCams {
+    EDL_RUCAM, EDL_RDCAM, EDL_DDCAM, EDL_PUCAM1, EDL_PUCAM2, NAVCAM_LEFT, NAVCAM_RIGHT, MCZ_RIGHT, MCZ_LEFT, FRONT_HAZCAM_LEFT_A, FRONT_HAZCAM_RIGHT_A, REAR_HAZCAM_LEFT, REAR_HAZCAM_RIGHT, SKYCAM, SHERLOC_WATSON
+}
+
+enum class CuriosityCams {
+    FHAZ, RHAZ, MAST, CHEMCAM, MAHLI, MARDI, NAVCAM
+}
+
+enum class OpportunityCams {
+    FHAZ, RHAZ, NAVCAM, PANCAM, MINITES
+}
+
+enum class SpiritCams {
+    FHAZ, RHAZ, NAVCAM, PANCAM, MINITES
+}
