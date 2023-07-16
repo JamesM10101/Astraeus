4fd81173eedaa686cf35c48502c8df80750529d2
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-07-16 14:46:11 -0400

    HomeViewModel created

---
+++ app/src/main/java/com/jamesm10101/astraeus/viewModels/HomeViewModel.kt
@@ -0,0 +1,133 @@
+package com.jamesm10101.astraeus.viewModels
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jamesm10101.astraeus.apis.EpicAPI
+import com.jamesm10101.astraeus.apis.MarsRoverAPI
+import com.jamesm10101.astraeus.apis.PlanetaryAPI
+import com.jamesm10101.astraeus.data.APOD
+import com.jamesm10101.astraeus.data.Epic
+import com.jamesm10101.astraeus.data.EpicCollection
+import com.jamesm10101.astraeus.data.EpicImageType
+import com.jamesm10101.astraeus.data.MarsRoverLatestPhotos
+import com.jamesm10101.astraeus.data.MarsRoverName
+import com.jamesm10101.astraeus.utils.epicImageConverter
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+class HomeViewModel : ViewModel() {
+
+    private val _apodResult = MutableLiveData<APOD>()
+    private val _epicLatest = MutableLiveData<List<Epic>>()
+    private val _rover1Name = MutableLiveData<String>()
+    private val _rover2Name = MutableLiveData<String>()
+    private val _rover1photos = MutableLiveData<MarsRoverLatestPhotos>()
+    private val _rover2photos = MutableLiveData<MarsRoverLatestPhotos>()
+
+    // accessors
+    val apodResult: LiveData<APOD> = _apodResult
+    val epicLatest: LiveData<List<Epic>> = _epicLatest
+    val rover1Name: LiveData<String> = _rover1Name
+    val rover2Name: LiveData<String> = _rover2Name
+    val rover1photos: LiveData<MarsRoverLatestPhotos> = _rover1photos
+    val rover2photos: LiveData<MarsRoverLatestPhotos> = _rover2photos
+
+    init {
+        getCurrentAPOD(_apodResult)
+        getEpicLatest(_epicLatest, EpicCollection.ENHANCED, EpicImageType.JPG)
+        getRovers()
+    }
+
+    /**
+     * Sets the rover name and photos variables.
+     *
+     */
+    private fun getRovers() {
+        // Rover 1 should be perseverance
+        _rover1Name.value = MarsRoverName.PERSEVERANCE.name.lowercase().replaceFirstChar {
+            it.uppercase()
+        }
+        getRoverLatest(MarsRoverName.PERSEVERANCE, _rover1photos)
+
+        // get a random rover for the second one
+        var randomRover = MarsRoverName.values()[Random.nextInt(0, MarsRoverName.values().size)]
+        while (randomRover == MarsRoverName.PERSEVERANCE) {
+            randomRover = MarsRoverName.values()[Random.nextInt(0, MarsRoverName.values().size)]
+        }
+
+        _rover2Name.value = randomRover.name.lowercase().replaceFirstChar {
+            it.uppercase()
+        }
+        getRoverLatest(randomRover, _rover2photos)
+    }
+
+    /**
+     * Grabs the latest photos form NASA's EPIC api and formats the urls.
+     *
+     * @param epicList A mutable live data object that stores the list of images
+     * @param collType The type of collection to retrieve from EPIC
+     * @param imgType The type of image to retrieve from EPIC
+     */
+    private fun getEpicLatest(
+        epicList: MutableLiveData<List<Epic>>,
+        collType: EpicCollection,
+        imgType: EpicImageType
+    ) {
+        viewModelScope.launch {
+            try {
+                epicList.value =
+                    EpicAPI.retrofitService.getLatest(collType.name.lowercase())
+
+                // format the urls
+                epicList.value!!.forEach {
+                    it.imgSrcUrl =
+                        epicImageConverter(it.image, collType, imgType)
+                }
+
+            } catch (e: java.lang.Exception) {
+                Log.e("GetEpicLatest", e.message.toString())
+            }
+        }
+    }
+
+    /**
+     * Gets the latest info and images about the specified Mars rover from NASA's Mars Rover API.
+     *
+     * @param roverName The name of the Mars rover
+     * @param roverDetails A mutable live data object that stores the rover details
+     * @param roverPhotos A mutable live data object that stores the latest photos form the rover
+     */
+    private fun getRoverLatest(
+        roverName: MarsRoverName,
+        roverPhotos: MutableLiveData<MarsRoverLatestPhotos>
+    ) {
+        viewModelScope.launch {
+            try {
+                roverPhotos.value =
+                    MarsRoverAPI.retrofitService.getRoverLatestPhotos(roverName.name)
+            } catch (e: Exception) {
+                Log.e("GetRoverLatest", e.message.toString())
+            }
+        }
+    }
+
+    /**
+     * Gets the latest apod from NASA's APOD API
+     *
+     * @param apodResult A mutable live data object that stores the apod
+     */
+    private fun getCurrentAPOD(
+        apodResult: MutableLiveData<APOD>
+    ) {
+        viewModelScope.launch {
+            try {
+                apodResult.value = PlanetaryAPI.retrofitService.getCurrentApod()
+            } catch (e: Exception) {
+                Log.e("GetCurrentAPOD", e.message.toString())
+            }
+        }
+    }
+}
