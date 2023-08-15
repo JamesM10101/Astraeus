7e5379e9bfdebc7330ec71ede1b62a70a87df347
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-08-14 23:00:57 -0400

    ConstructionFragment created

---
+++ app/src/main/java/com/jamesm10101/astraeus/views/ConstructionFragment.kt
@@ -0,0 +1,19 @@
+package com.jamesm10101.astraeus.views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jamesm10101.astraeus.R
+
+class ConstructionFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_construction, container, false)
+    }
+
+}
