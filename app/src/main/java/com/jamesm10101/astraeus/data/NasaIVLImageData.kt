ae2709ef673f9e84b741c45b773d5fe448cc10aa
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-08-25 12:20:35 -0400

    NasaIVLImageData class updated
    Added:
    - secondaryCreator
    - description508

--- app/src/main/java/com/jamesm10101/astraeus/data/NasaIVLImageData.kt
+++ app/src/main/java/com/jamesm10101/astraeus/data/NasaIVLImageData.kt
@@ -6,10 +6,12 @@ data class NasaIVLImageData(
     @Json(name = "center") val center: String,
     @Json(name = "title") val title: String,
     @Json(name = "photographer") val photographer: String?,
+    @Json(name = "secondary_creator") val secondaryCreator: String?,
     @Json(name = "keywords") val keywords: List<String>?,
     @Json(name = "location") val location: String?,
     @Json(name = "nasa_id") val nasaId: String,
     @Json(name = "media_type") val mediaType: String,
     @Json(name = "date_created") val dateCreated: String,
     @Json(name = "description") val description: String,
+    @Json(name = "description_508") val description508: String?,
 )
