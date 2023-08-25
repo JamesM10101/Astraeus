0afcd41fcca38b5457a9be618a7f649eb833119d
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-08-25 17:08:50 -0400

    IVLSearchFragment updated
    Added time created

--- app/src/main/java/com/jamesm10101/astraeus/views/IVLSearchResultFragment.kt
+++ app/src/main/java/com/jamesm10101/astraeus/views/IVLSearchResultFragment.kt
@@ -1,19 +1,22 @@
 package com.jamesm10101.astraeus.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
 import com.jamesm10101.astraeus.R
-import com.jamesm10101.astraeus.adapter.RecyclerItemTouchListener
 import com.jamesm10101.astraeus.data.NasaIVLImageCollection
 import com.jamesm10101.astraeus.databinding.FragmentIvlSearchResultBinding
 import com.jamesm10101.astraeus.viewModels.IVLSearchResultViewModel
 import com.jamesm10101.astraeus.viewModels.MainViewModel
+import java.text.SimpleDateFormat
+import java.util.Locale
+import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities.Local
 
 private const val ARG_SEARCH_RESULT = "searchResult"
 
@@ -41,6 +44,21 @@ class IVLSearchResultFragment : Fragment() {
         binding.searchResult = searchResult?.images!![0]
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val txtDateCreated = view.findViewById<TextView>(R.id.txtV_created)
+
+        try {
+            val date = searchResult!!.images[0].data[0].dateCreated
+            txtDateCreated.text = SimpleDateFormat("MMMM dd yyyy", Locale.US).format(
+                SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US).parse(date)!!
+            );
+        } catch (e: Exception) {
+            Log.e("setCreatedDate", e.message.toString())
+        }
     }
 
     companion object {
