60c5ab634b6ba771a5ff4a1e626aa3a8484ed90d
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-09-12 23:34:32 -0400

    EpicPhoto fragment and viewModel updated
    - Save button added

--- app/src/main/java/com/jamesm10101/astraeus/viewModels/EpicPhotoViewModel.kt
+++ app/src/main/java/com/jamesm10101/astraeus/viewModels/EpicPhotoViewModel.kt
@@ -5,13 +5,37 @@ import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.jamesm10101.astraeus.data.Epic
+import com.jamesm10101.astraeus.databases.SavedEpicDao
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
-class EpicPhotoViewModel : ViewModel() {
+class EpicPhotoViewModel(
+    val epic: Epic, private val epicDao: SavedEpicDao
+) : ViewModel() {
 
     private val _showEpicDetails = MutableLiveData<Boolean>()
-
-    // accessors
     val showEpicDetails: LiveData<Boolean> = _showEpicDetails
+
+    val epicSaved = epicDao.getSavedEpicFromId(epic.id)
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), null)
+
+    /**
+     * Saves an Epic if it does not exist, deletes if it does.
+     */
+    fun handleBookmarkClick(): View.OnClickListener {
+        return View.OnClickListener {
+            viewModelScope.launch {
+                when (epicSaved.value != null) {
+                    true -> epicDao.deleteEpic(epicSaved.value!!)
+                    false -> epicDao.saveEpic(epic)
+                }
+            }
+        }
+    }
 
     /**
      * Toggles the visibility of the image details component
@@ -26,5 +50,16 @@ class EpicPhotoViewModel : ViewModel() {
                 Log.e("showEpicDetails", e.message.toString())
             }
         }
+    }
+}
+
+class EpicPhotoViewModelFactory(
+    private val epic: Epic, private val savedEpicDao: SavedEpicDao
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(EpicPhotoViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST") return EpicPhotoViewModel(epic, savedEpicDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
--- app/src/main/java/com/jamesm10101/astraeus/views/EpicPhotoFragment.kt
+++ app/src/main/java/com/jamesm10101/astraeus/views/EpicPhotoFragment.kt
@@ -4,34 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import com.jamesm10101.astraeus.data.Epic
 import com.jamesm10101.astraeus.databinding.FragmentEpicPhotoBinding
 import com.jamesm10101.astraeus.viewModels.EpicPhotoViewModel
+import com.jamesm10101.astraeus.viewModels.EpicPhotoViewModelFactory
 import com.jamesm10101.astraeus.viewModels.MainViewModel
 
 class EpicPhotoFragment : MainBaseFragment() {
 
-    private val viewModel: EpicPhotoViewModel by viewModels()
     private lateinit var mainViewModel: MainViewModel
+    private lateinit var viewModel: EpicPhotoViewModel
+
+    private var epic: Epic? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         handleBackPress(mainViewModel)
+
+        @Suppress("DEPRECATION")
+        epic = requireArguments().getParcelable("epicPhoto")
+
+        viewModel = ViewModelProvider(
+            this, EpicPhotoViewModelFactory(
+                epic!!,
+                (requireActivity().application as AstraeusApplication).savedPostsDatabase.savedEpicDao
+            )
+        )[EpicPhotoViewModel::class.java]
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         val binding = FragmentEpicPhotoBinding.inflate(inflater)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         binding.mainViewModel = mainViewModel
-
-        @Suppress("DEPRECATION")
-        binding.epicPhoto = requireArguments().getParcelable("epicPhoto")
 
         return binding.root
     }
