60432a7f7e37755f75588ab08314b8d2bed01161
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-09-14 23:55:21 -0400

    SavedIVLImageDao updated
    Added:
     - getSavedIvlFromId function
     - ascending and descending functions

--- app/src/main/java/com/jamesm10101/astraeus/databases/SavedIVLImageDao.kt
+++ app/src/main/java/com/jamesm10101/astraeus/databases/SavedIVLImageDao.kt
@@ -30,12 +30,29 @@ interface SavedIVLImageDao {
     suspend fun deleteIVLImage(nasaIVLImage: NasaIVLImageData)
 
     /**
+     * Gets a saved NasaIVLImageData with a matching id if one exists.
+     *
+     * @param id The id of the NasaIVLImageData to check for.
+     * @return The saved NasaIVLImageData if one exists, null otherwise.
+     */
+    @Query("SELECT * FROM ivl_images WHERE nasaId = :id")
+    fun getSavedIvlFromId(id: String): Flow<NasaIVLImageData?>
+
+    /**
      * Gets all saved IVL images ordered by ID.
      *
      * @return All saved IVL images ordered by ID.
      */
     @Query("SELECT * FROM ivl_images ORDER BY nasaId ASC")
-    fun getAllIVLOrderById(): Flow<List<NasaIVLImageData>>
+    fun getAllIVLOrderByIdAsc(): Flow<List<NasaIVLImageData>>
+
+    /**
+     * Gets all saved IVL images ordered by ID.
+     *
+     * @return All saved IVL images ordered by ID.
+     */
+    @Query("SELECT * FROM ivl_images ORDER BY nasaId DESC")
+    fun getAllIVLOrderByIdDesc(): Flow<List<NasaIVLImageData>>
 
     /**
      * Gets all saved IVL images containing a specific keyword ordered by ID.
@@ -43,6 +60,14 @@ interface SavedIVLImageDao {
      * @return All saved IVL images containing a specific keyword ordered by ID.
      */
     @Query("SELECT * FROM ivl_images WHERE keywords LIKE :keyword ORDER BY nasaId ASC")
-    fun getAllIVLByKeywordOrderById(keyword: String): Flow<List<NasaIVLImageData>>
+    fun getAllIVLByKeywordOrderByIdAsc(keyword: String): Flow<List<NasaIVLImageData>>
+
+    /**
+     * Gets all saved IVL images containing a specific keyword ordered by ID.
+     *
+     * @return All saved IVL images containing a specific keyword ordered by ID.
+     */
+    @Query("SELECT * FROM ivl_images WHERE keywords LIKE :keyword ORDER BY nasaId DESC")
+    fun getAllIVLByKeywordOrderByIdDesc(keyword: String): Flow<List<NasaIVLImageData>>
 
 }
