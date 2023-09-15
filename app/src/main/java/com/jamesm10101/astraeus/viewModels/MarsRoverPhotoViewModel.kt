3085d9c78d607002cfd86d79e49ba7d695c9014d
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-09-14 22:02:59 -0400

    MarsRoverPhoto fragment and viewModel updated
    Added save button

--- app/src/main/java/com/jamesm10101/astraeus/viewModels/MarsRoverPhotoViewModel.kt
+++ app/src/main/java/com/jamesm10101/astraeus/viewModels/MarsRoverPhotoViewModel.kt
@@ -9,17 +9,41 @@ import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.jamesm10101.astraeus.R
+import com.jamesm10101.astraeus.data.MarsRoverPhoto
+import com.jamesm10101.astraeus.databases.SavedMarsRoverPhotoDao
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import java.lang.Exception
 
-class MarsRoverPhotoViewModel : ViewModel() {
-
-    lateinit var resources: Resources
+class MarsRoverPhotoViewModel(
+    val marsRoverPhoto: MarsRoverPhoto,
+    private val marsRoverPhotoDao: SavedMarsRoverPhotoDao,
+    private val resources: Resources,
+) : ViewModel() {
 
     private val _showRoverDetails = MutableLiveData<Boolean>()
-
-    // accessors
     val showRoverDetails: LiveData<Boolean> = _showRoverDetails
+
+    val photoSaved = marsRoverPhotoDao.getSavedPhotoFromId(marsRoverPhoto.id)
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), null)
+
+    /**
+     * Saves a MarsRoverPhoto if it does not exist, deletes it if does.
+     *
+     * @param marsRoverPhoto The MarsRoverPhoto to save/delete.
+     */
+    fun handleBookmarkClick(marsRoverPhoto: MarsRoverPhoto) {
+        viewModelScope.launch {
+            when (photoSaved.value != null) {
+                true -> marsRoverPhotoDao.deleteMarsRoverPhoto(photoSaved.value!!)
+                false -> marsRoverPhotoDao.saveMarsRoverPhoto(marsRoverPhoto)
+            }
+        }
+    }
 
     /**
      * Creates an onClickListener that exposes the rovers details
@@ -52,3 +76,17 @@ class MarsRoverPhotoViewModel : ViewModel() {
 
 }
 
+class MarsRoverPhotoViewModelFactory(
+    private val marsRoverPhoto: MarsRoverPhoto,
+    private val marsRoverPhotoDao: SavedMarsRoverPhotoDao,
+    private val resources: Resources
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MarsRoverPhotoViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST") return MarsRoverPhotoViewModel(
+                marsRoverPhoto, marsRoverPhotoDao, resources
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
--- app/src/main/java/com/jamesm10101/astraeus/views/MarsRoverPhotoFragment.kt
+++ app/src/main/java/com/jamesm10101/astraeus/views/MarsRoverPhotoFragment.kt
@@ -4,21 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import com.jamesm10101.astraeus.data.MarsRoverPhoto
 import com.jamesm10101.astraeus.databinding.FragmentMarsRoverPhotoBinding
 import com.jamesm10101.astraeus.viewModels.MainViewModel
 import com.jamesm10101.astraeus.viewModels.MarsRoverPhotoViewModel
+import com.jamesm10101.astraeus.viewModels.MarsRoverPhotoViewModelFactory
 
 class MarsRoverPhotoFragment : MainBaseFragment() {
 
-    private val viewModel: MarsRoverPhotoViewModel by viewModels()
+    private lateinit var viewModel: MarsRoverPhotoViewModel
     private lateinit var mainViewModel: MainViewModel
+
+    private var marsRoverPhoto: MarsRoverPhoto? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         handleBackPress(mainViewModel)
+
+        @Suppress("DEPRECATION")
+        marsRoverPhoto = requireArguments().getParcelable("marsRoverPhoto")
+
+        viewModel = ViewModelProvider(
+            this, MarsRoverPhotoViewModelFactory(
+                marsRoverPhoto!!,
+                (requireActivity().application as AstraeusApplication).savedPostsDatabase.savedMarsRoverPhotoDao,
+                resources
+            )
+        )[MarsRoverPhotoViewModel::class.java]
     }
 
     override fun onCreateView(
@@ -28,10 +42,6 @@ class MarsRoverPhotoFragment : MainBaseFragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.mainViewModel = mainViewModel
         binding.viewModel = viewModel
-        viewModel.resources = resources
-
-        @Suppress("DEPRECATION")
-        binding.roverPhoto = requireArguments().getParcelable("marsRoverPhoto")
 
         return binding.root
     }
