69d3a7276fa456ee8bc70d943d3f43157d17fd2a
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-09-14 23:56:25 -0400

    IvlSearchResult fragment and viewmodel updated
    Added save button

--- app/src/main/java/com/jamesm10101/astraeus/viewModels/IVLSearchResultViewModel.kt
+++ app/src/main/java/com/jamesm10101/astraeus/viewModels/IVLSearchResultViewModel.kt
@@ -4,11 +4,39 @@ import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.jamesm10101.astraeus.data.NasaIVLImage
+import com.jamesm10101.astraeus.data.NasaIVLImageData
+import com.jamesm10101.astraeus.databases.SavedIVLImageDao
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
-class IVLSearchResultViewModel : ViewModel() {
+class IVLSearchResultViewModel(
+    val ivlImage: NasaIVLImage, private val ivlImageDao: SavedIVLImageDao
+) : ViewModel() {
+
     private val _showFullDescription = MutableLiveData<Boolean>(false)
-
     val showFullDetails: LiveData<Boolean> = _showFullDescription
+
+    private val ivlImageData: NasaIVLImageData = ivlImage.data[0]
+    val ivlSaved = ivlImageDao.getSavedIvlFromId(ivlImageData.nasaId)
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), null)
+
+    /**
+     * Saves the NasaIVLImageData if it does not exist, deletes it if does.
+     */
+    fun handleBookmarkClick(): View.OnClickListener {
+        return View.OnClickListener {
+            viewModelScope.launch {
+                when (ivlSaved.value != null) {
+                    true -> ivlImageDao.deleteIVLImage(ivlSaved.value!!)
+                    false -> ivlImageDao.saveIVLImage(ivlImageData)
+                }
+            }
+        }
+    }
 
     /**
      * Toggles showFullDescription, used to toggle the max line count.
@@ -27,5 +55,18 @@ class IVLSearchResultViewModel : ViewModel() {
         return View.OnClickListener {
             toggleShowFullDetails()
         }
+    }
+}
+
+class IVLSearchResultViewModelFactory(
+    private val ivlImage: NasaIVLImage, private val ivlImageDao: SavedIVLImageDao
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(IVLSearchResultViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST") return IVLSearchResultViewModel(
+                ivlImage, ivlImageDao
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
--- app/src/main/java/com/jamesm10101/astraeus/views/IVLSearchResultFragment.kt
+++ app/src/main/java/com/jamesm10101/astraeus/views/IVLSearchResultFragment.kt
@@ -6,12 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.jamesm10101.astraeus.R
 import com.jamesm10101.astraeus.data.NasaIVLImage
 import com.jamesm10101.astraeus.databinding.FragmentIvlSearchResultBinding
 import com.jamesm10101.astraeus.viewModels.IVLSearchResultViewModel
+import com.jamesm10101.astraeus.viewModels.IVLSearchResultViewModelFactory
 import com.jamesm10101.astraeus.viewModels.MainViewModel
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -20,27 +20,33 @@ private const val ARG_SEARCH_RESULT = "searchResult"
 
 class IVLSearchResultFragment : MainBaseFragment() {
     private var searchResult: NasaIVLImage? = null
-    private val viewModel: IVLSearchResultViewModel by viewModels()
+    private lateinit var viewModel: IVLSearchResultViewModel
     private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            searchResult = it.getParcelable(ARG_SEARCH_RESULT)
-        }
+
+        @Suppress("DEPRECATION")
+        searchResult = requireArguments().getParcelable(ARG_SEARCH_RESULT)
+
         mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         handleBackPress(mainViewModel)
+
+        viewModel = ViewModelProvider(
+            this, IVLSearchResultViewModelFactory(
+                searchResult!!,
+                (requireActivity().application as AstraeusApplication).savedPostsDatabase.savedIVLImageDao
+            )
+        )[IVLSearchResultViewModel::class.java]
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         val binding = FragmentIvlSearchResultBinding.inflate(inflater)
         binding.lifecycleOwner = viewLifecycleOwner
         binding.mainViewModel = mainViewModel
         binding.viewModel = viewModel
-        binding.searchResult = searchResult
 
         return binding.root
     }
@@ -54,7 +60,7 @@ class IVLSearchResultFragment : MainBaseFragment() {
             val date = searchResult!!.data[0].dateCreated
             txtDateCreated.text = SimpleDateFormat("MMMM dd yyyy", Locale.US).format(
                 SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US).parse(date)!!
-            );
+            )
         } catch (e: Exception) {
             Log.e("setCreatedDate", e.message.toString())
         }
@@ -68,19 +74,11 @@ class IVLSearchResultFragment : MainBaseFragment() {
          * @param searchResult The search result item
          * @return A new instance of fragment IVLSearchResultFragment.
          */
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param searchResult The search result item
-         * @return A new instance of fragment IVLSearchResultFragment.
-         */
         @JvmStatic
-        fun newInstance(searchResult: String) =
-            IVLSearchResultFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_SEARCH_RESULT, searchResult)
-                }
+        fun newInstance(searchResult: String) = IVLSearchResultFragment().apply {
+            arguments = Bundle().apply {
+                putString(ARG_SEARCH_RESULT, searchResult)
             }
+        }
     }
 }
